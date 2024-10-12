f60e13129f42c9436bb6037677563c0094f36dc8
Bartek <bartekk.gordon@gmail.com>
2024-10-12 21:07:22 +0200

    Move utility asset from PaperBlades

---
+++ Assets/Scripts/Utility/MissingScriptUtility.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace PlaceHolders.Utility
+{
+    public class MissingScriptUtility : MonoBehaviour
+    {
+        [MenuItem("Tools/Search for missing scripts")]
+        public static void SearchForMissingScripts()
+        {
+            FindMissingScriptsInScene();
+            FindMissingScriptsInProject();
+        }
+
+        private static void FindMissingScriptsInProject()
+        {
+            string[] prefabPaths = AssetDatabase.GetAllAssetPaths().Where(path => path.EndsWith(".prefab", System.StringComparison.OrdinalIgnoreCase)).ToArray();
+
+            foreach (string prefabPath in prefabPaths)
+            {
+                GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(prefabPath);
+
+                foreach (Component component in prefab.GetComponentsInChildren<Component>())
+                {
+                    if (component == null)
+                    {
+                        Debug.LogError("Prefab found with missing script " + prefabPath, prefab);
+                    }
+                }
+            }
+        }
+        private static void FindMissingScriptsInScene()
+        {
+            foreach (GameObject gameObject in FindObjectsByType<GameObject>(FindObjectsSortMode.None))
+            {
+                foreach (Component component in gameObject.GetComponentsInChildren<Component>())
+                {
+                    if (component == null)
+                    {
+                        Debug.LogError("GameObject found with missing script " + gameObject.name);
+                    }
+                }
+            }
+        }
+    }
+}
