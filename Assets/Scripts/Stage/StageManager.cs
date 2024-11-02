4dcaa8d64c5b7db5d542bb1f45aab05970edc96a
Bartek <bartekk.gordon@gmail.com>
2024-11-02 10:08:18 +0100

    Bulk spawning

--- Assets/Scripts/Stage/StageManager.cs
+++ Assets/Scripts/Stage/StageManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,12 +8,41 @@ public class StageManager : MonoBehaviour
     [SerializeField] Transform spawnPoint;
     [SerializeField] SplineManager SplineManager;
     [SerializeField] List<UnitBlueprint> UnitBlueprints;
+    [Header("Spawning times")]
+    [SerializeField] float initialWaitTime;
+    [SerializeField] float waitBetweenUnit;
+    [SerializeField] float bulkWait;
 
-    void Update()
+    void Start()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        Initialize(UnitBlueprints: null);
+    }
+
+    public void Initialize(List<UnitBlueprint> UnitBlueprints)
+    {
+        StartCoroutine(SpawningRoutine());
+    }
+
+    IEnumerator SpawningRoutine()
+    {
+        yield return new WaitForSeconds(initialWaitTime);
+
+        foreach (var unit in UnitBlueprints)
         {
-            SpawnUnit(UnitBlueprints[0]);
+            if (unit.Config.BulkSpawnCount > 1)
+            {
+                for (int i = 0; i < unit.Config.BulkSpawnCount; i++)
+                {
+                    SpawnUnit(unit);
+                    yield return new WaitForSeconds(bulkWait);
+                }
+                
+            }
+            else
+            {
+                SpawnUnit(unit);
+            }
+            yield return new WaitForSeconds(waitBetweenUnit);
         }
     }
 
